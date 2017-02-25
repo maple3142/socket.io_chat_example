6ceb1c8c8ee398f8c01f632baadc5e5fbf6c3ac5
maple3142 <kirby741852963@gmail.com>
2017-02-25 10:11:32 +0800

    get js check and repl

--- index.js
+++ index.js
@@ -10,18 +10,34 @@ console.log=t=>{
 	log(t);
 	fs.appendFile("./log.txt",t+'\n',err=>{}); 
 };
+var error=console.error;
+console.error=t=>{
+	error(t);
+	fs.appendFile("./error.txt",t+'\n',err=>{}); 
+};
+
 app.get('/',(req,res)=>{
   res.sendFile(path.join(__dirname,'index.html'));
 });
 app.get('/js/:js',(req,res)=>{
-  res.sendFile(path.join(path.join(__dirname,'js'),req.params.js));
+  var d=path.join(path.join(__dirname,'js'),req.params.js);
+  fs.exists(d,exist=>{
+    if(exist)res.sendFile(path.join(path.join(__dirname,'js'),req.params.js));
+    else{
+      res.status(404);
+      res.send('<h1>File not found!</h1>');
+    }
+  });
+});
+app.all('*',(req,res)=>{
+  res.redirect('http://maple3142.net/');
 });
 app.listen(80,()=>console.log('server listen on *80'));
 
 io.on('connection',(socket)=>{
   socket.on('userjoined',msg=>{
     msg=JSON.parse(msg);
-    console.log(msg.nick+' joined '+Date.now());
+    console.log(msg.nick+' joined '+Date.now()+' '+socket.request.connection.remoteAddress);
     io.emit('userjoined',JSON.stringify({
         nick: msg.nick,
         h: new Date().getHours(),
@@ -30,7 +46,7 @@ io.on('connection',(socket)=>{
   });
   socket.on('userleaved',msg=>{
     msg=JSON.parse(msg);
-    console.log(msg.nick+' leaved '+Date.now());
+    console.log(msg.nick+' leaved '+Date.now()+' '+socket.request.connection.remoteAddress);
     io.emit('userleaved',JSON.stringify({
         nick: msg.nick,
         h: new Date().getHours(),
@@ -63,4 +79,20 @@ schedule.scheduleJob(rule,()=>{
 
 http.listen(8080,()=>{
   console.log('socket server listen on *8080');
+});
+
+var rl=require('readline');
+var l=rl.createInterface({
+  input: process.stdin
+});
+l.on('line',c=>{
+  try{
+    log('---EVAL RESULT START---');
+    log('INPUT: '+c)
+    log(eval(c));
+    log('---EVAL RESULT END-----');
+  }catch(e){
+    console.error('---EVAL EXCEPTION---');
+    console.error(e);
+  }
 });
